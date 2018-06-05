0f34be8716dfec5bbc207cb1a25a03724bcb995e
nithin <nithinoffcl@gmail.com>
2018-06-05 20:25:07 +0000

    third commit

--- LendingBooks/settings.py
+++ LendingBooks/settings.py
@@ -143,8 +143,6 @@ DATABASES = {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
-
-    DATABASES = {
   'default': {
       'ENGINE': 'django.db.backends.postgresql_psycopg2',
       'NAME': 'mydb',
@@ -154,7 +152,7 @@ DATABASES = {
       'PORT': '',
   }
 }
-}
+
 
 
 # Password validation
