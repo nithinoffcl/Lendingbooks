7af092d3c1e67dba4c93bf735e75e0549d3e3b1c
nithinoffcl <32201172+nithinoffcl@users.noreply.github.com>
2018-08-12 16:05:04 +0530

    Update admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User,Order,Login
+from .models import User,Order,Login,Inventory
 
 
 admin.site.register(User)
 admin.site.register(Order)
 admin.site.register(Login)
+admin.site.register(Inventory)
