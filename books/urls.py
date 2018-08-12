d9d7735ca952091e2b158d6dbdc731b9355402e0
nithinoffcl <32201172+nithinoffcl@users.noreply.github.com>
2018-08-12 16:03:37 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.login,name = "login"),
     path('signup/',views.signup,name = "signup"),
     path('temp/',views.temp,name = "temp"),
+    path('adminpage/',views.admin,name="admin"),
     path('requestconfirmation/<int:value>/',views.requestconfirmation,name = "requestconfirmation"),
 
 ]
